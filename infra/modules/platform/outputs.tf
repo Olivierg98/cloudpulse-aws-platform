acbbffe5066fd63a7830565920458fe9b93a3fb4
Olivierg98 <olivierg_98@hotmail.co.uk>
2026-09-20 22:16:16 +0100

    feat: upgrade infra/modules/platform/outputs.tf

--- infra/modules/platform/outputs.tf
+++ infra/modules/platform/outputs.tf
@@ -1,5 +1,5 @@
 output "url" {
-  value = "http://${aws_lb.this.dns_name}"
+  value = "${var.certificate_arn == null ? "http" : "https"}://${aws_lb.this.dns_name}"
 }
 output "vpc_id" {
   value = aws_vpc.this.id
@@ -9,4 +9,17 @@ output "autoscaling_group" {
 }
 output "dashboard" {
   value = aws_cloudwatch_dashboard.this.dashboard_name
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "database_secret_arn" {
+  value     = try(aws_secretsmanager_secret.database[0].arn, null)
+  sensitive = true
+}
+
+output "alarm_topic_arn" {
+  value = try(aws_sns_topic.alerts[0].arn, null)
 }
